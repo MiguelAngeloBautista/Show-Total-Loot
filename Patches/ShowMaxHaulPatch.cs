9697b690f6aa177fd43a3ef78f0386d156fd985d
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2025-04-17 00:22:53 +0930

    Add ability to see the max haul goal of the level - Added a Label to Game HUD which shows the haulGoalMax variable of RoundDirector - Updates the max haul goal based on any lost value on valuables

---
+++ Patches/ShowMaxHaulPatch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using MenuLib;
+using MenuLib.MonoBehaviors;
+namespace ShowTotalLoot.Patches;
+
+[HarmonyPatch(typeof(RoundDirector))]
+public class ShowMaxHaulPatch
+{
+    private static int _currentMaxHaul = 0;
+    private static Transform? _gameHud;
+    private static REPOLabel? _maxHaulGoalLabel;
+    
+    [HarmonyPostfix, HarmonyPatch(typeof(ExtractionPoint), nameof(ExtractionPoint.ActivateTheFirstExtractionPointAutomaticallyWhenAPlayerLeaveTruck))]
+    private static void StartRound_Postfix(ExtractionPoint __instance)
+    {
+        ShowTotalLoot.Logger.LogInfo($"{__instance} Start Postfix");
+
+        _currentMaxHaul = RoundDirector.instance.haulGoalMax;
+
+        UpdateLabel(_currentMaxHaul);
+        
+        ShowTotalLoot.Logger.LogInfo($"Round Max Haul: {_currentMaxHaul}");
+    
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(PhysGrabObjectImpactDetector), nameof(PhysGrabObjectImpactDetector.BreakRPC))]
+    private static void BreakRPC_Postfix(PhysGrabObjectImpactDetector __instance)
+    {
+        if (__instance.valuableObject)
+        {
+            ShowTotalLoot.Logger.LogInfo($"{__instance} Start Postfix");
+
+            float valueLost = __instance.valuableObject.dollarValueOriginal - __instance.valuableObject.dollarValueCurrent;
+            _currentMaxHaul -= (int)valueLost;
+        
+            UpdateLabel(_currentMaxHaul);
+        }
+        else
+        {
+            ShowTotalLoot.Logger.LogInfo($"{__instance} is not a ValuableObject");
+        }
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(LoadingUI), nameof(LoadingUI.StopLoading))]
+    private static void StopLoading_Postfix(LoadingUI __instance)
+    {
+        ShowTotalLoot.Logger.LogInfo($"{__instance} Start Postfix");
+        
+        _gameHud = GameObject.Find("Game Hud").transform;
+        _maxHaulGoalLabel = MenuAPI.CreateREPOLabel("Total Value: 0", _gameHud, localPosition: new Vector2(-260, -454));
+        ShowTotalLoot.Logger.LogInfo($"maxHaulGoalLabel {_maxHaulGoalLabel}");
+    }
+    
+    private static void UpdateLabel(int newMaxHaulGoal)
+    {
+        if (_maxHaulGoalLabel == null)
+        {
+            ShowTotalLoot.Logger.LogError("UpdateLabel(): MaxHaulGoalLabel is null");
+        }
+
+        ShowTotalLoot.Logger.LogInfo(_maxHaulGoalLabel);
+        ShowTotalLoot.Logger.LogInfo($"New Max Haul Goal: {newMaxHaulGoal}");
+        _maxHaulGoalLabel.labelTMP.text = $"Total Value: {newMaxHaulGoal}";
+        
+    }
+}
---
+++ ShowTotalLoot.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using MenuLib;
+using MenuLib.MonoBehaviors;
+using ShowTotalLoot.Patches;
+using Unity.VisualScripting;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ShowTotalLoot;
+
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+public class ShowTotalLoot : BaseUnityPlugin
+{
+    // Mod Details
+    private const string ModGUID = "ItsAGeBa.ShowTotalLoot";
+    private const string ModName = "Show Total Loot";
+    private const string ModVersion = "1.0.0";
+    
+    internal static ShowTotalLoot Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        Logger.LogInfo("Hallo This is the Show All Loot Mod");
+        
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+        
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+        
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+
+    private void Update()
+    {
+        // Code that runs every frame goes here
+    }
+}
